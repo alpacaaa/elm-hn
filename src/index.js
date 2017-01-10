c6abdae8996eec7f9b4bd2ac62b1f1a4251e8806
Marco Sampellegrini <babbonatale@alpacaaa.net>
2017-01-10 03:43:45 +0100

    Connect to Firebase.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 require('./main.css');
 
+var firebase = require('firebase')
+
+var app = firebase.initializeApp({
+  databaseURL: 'https://hacker-news.firebaseio.com/'
+})
+
+var db = firebase.database()
+db.ref('/v0/topstories').on('value', function(data) {
+  console.log(data.val())
+})
+
 var Elm = require('./Main.elm');
 
 var root = document.getElementById('root');
