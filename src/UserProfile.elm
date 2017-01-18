4968cd549935d984dc78da2ea089fb5aa24a7dfe
Marco Sampellegrini <babbonatale@alpacaaa.net>
2017-01-18 01:13:45 +0100

    Profile finished.

--- src/UserProfile.elm
+++ src/UserProfile.elm
@@ -1,7 +1,7 @@
 module UserProfile exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, href)
 import Types exposing (Context, User, Msg)
 import Utils exposing (formatTime, innerHtml)
 
@@ -13,6 +13,29 @@ aboutSection about =
     ]
 
 
+links : String -> List (Html Msg)
+links id =
+    [ dt [] []
+    , dd [] <|
+        List.map (makeLink id)
+            [ ( "submitted", "stories" )
+            , ( "threads", "comments" )
+            , ( "favorites", "favorites" )
+            ]
+    ]
+
+
+makeLink : String -> ( String, String ) -> Html Msg
+makeLink id ( slug, title ) =
+    let
+        url =
+            String.join "" [ "https://news.ycombinator.com/", slug, "?id=", id ]
+    in
+        div []
+            [ a [ href url ] [ text title ]
+            ]
+
+
 page : Context -> User -> Html Msg
 page { now } user =
     let
@@ -20,11 +43,12 @@ page { now } user =
             Maybe.map aboutSection user.about
                 |> Maybe.withDefault [ text "" ]
     in
-        div [ class "UserProfile" ]
+        div [ class "Comment__text UserProfile" ]
             [ h4 [] [ text user.id ]
             , dl [] <|
                 [ dt [] [ text "Created" ]
                 , dd [] [ text <| formatTime now user.created ]
                 ]
                     ++ about
+                    ++ links user.id
             ]
