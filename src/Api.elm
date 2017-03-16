f0b3a46aff3d263b5c9eed628d240df697ae8756
Marco Sampellegrini <babbonatale@alpacaaa.net>
2017-03-16 10:50:44 +0100

    Re organize some of the code.

--- src/Api.elm
+++ src/Api.elm
@@ -28,48 +28,147 @@ type alias Args =
     List ( String, String )
 
 
-field : String -> List Field -> Field
-field name fields =
-    Field name [] (Query fields)
+storiesQuery : StoryType -> Int -> Query
+storiesQuery storyType offset =
+    Query
+        [ field "hn"
+            [ fieldWithArgs (storyTypeString storyType)
+                [ ( "offset", toString offset ), ( "limit", "30" ) ]
+                [ field "id" []
+                , field "url" []
+                , field "title" []
+                , field "score" []
+                , field "time" []
+                , field "by"
+                    [ field "id" []
+                    ]
+                , field "descendants" []
+                , field "deleted" []
+                , field "dead" []
+                ]
+            ]
+        ]
 
 
-fieldWithArgs : String -> Args -> List Field -> Field
-fieldWithArgs name args fields =
-    Field name args (Query fields)
+storyQuery : String -> Query
+storyQuery id =
+    Query
+        [ field "hn"
+            [ fieldWithArgs "item"
+                [ ( "id", id ) ]
+                [ field "id" []
+                , field "url" []
+                , field "title" []
+                , field "score" []
+                , field "time" []
+                , field "by"
+                    [ field "id" []
+                    ]
+                , fetchKids 5
+                , field "deleted" []
+                , field "dead" []
+                ]
+            ]
+        ]
 
 
-queryToString : Query -> String
-queryToString (Query query) =
-    if List.isEmpty query then
-        ""
-    else
-        let
-            str =
-                List.map fieldToString query
-                    |> List.foldr (++) ""
-        in
-            "{ " ++ str ++ " }"
+userQuery : String -> Query
+userQuery id =
+    Query
+        [ field "hn"
+            [ fieldWithArgs "user"
+                [ ( "id", toString id ) ]
+                [ field "id" []
+                , field "created" []
+                , field "about" []
+                ]
+            ]
+        ]
 
 
-fieldToString : Field -> String
-fieldToString { name, args, query } =
-    name ++ " " ++ argsToString args ++ " " ++ queryToString query
+commentFields : List Field
+commentFields =
+    [ field "id" []
+    , field "text" []
+    , field "score" []
+    , field "time" []
+    , field "by"
+        [ field "id" []
+        ]
+    , field "deleted" []
+    , field "dead" []
+    ]
 
 
-argsToString : Args -> String
-argsToString args =
-    if List.isEmpty args then
-        ""
-    else
-        let
-            transform =
-                \( key, value ) -> key ++ ": " ++ value
+fetchStories : StoryType -> Int -> Http.Request (List Story)
+fetchStories storyType offset =
+    let
+        query =
+            queryToString <| storiesQuery storyType offset
 
-            str =
-                List.map transform args
-                    |> List.foldr (++) " "
-        in
-            "( " ++ str ++ " )"
+        jsonBody =
+            Http.jsonBody <|
+                Encode.object
+                    [ ( "query", Encode.string query ) ]
+
+        skipNull =
+            Decode.map MaybeX.values
+
+        finalDecoder =
+            Decode.list (Decode.nullable storyDecoder)
+                |> skipNull
+                |> skipDeleted
+
+        decoder =
+            Decode.at [ "data", "hn", (storyTypeString storyType) ] finalDecoder
+    in
+        Http.post "https://www.graphqlhub.com/graphql" jsonBody decoder
+
+
+fetchKids : Int -> Field
+fetchKids count =
+    let
+        fields =
+            if count == 0 then
+                commentFields
+            else
+                fetchKids (count - 1) :: commentFields
+    in
+        field "kids" fields
+
+
+fetchStory : String -> Http.Request Story
+fetchStory id =
+    let
+        query =
+            queryToString <| storyQuery id
+
+        jsonBody =
+            Http.jsonBody <|
+                Encode.object
+                    [ ( "query", Encode.string query ) ]
+
+        decoder =
+            Decode.at [ "data", "hn", "item" ] storyDecoder
+    in
+        Http.post "https://www.graphqlhub.com/graphql" jsonBody decoder
+
+
+fetchUser : String -> Http.Request User
+fetchUser id =
+    let
+        query =
+            queryToString <| userQuery id
+
+        jsonBody =
+            Http.jsonBody <|
+                Encode.object
+                    [ ( "query", Encode.string query ) ]
+
+        decoder =
+            Decode.at [ "data", "hn", "user" ] userDecoder
+    in
+        Http.post "https://www.graphqlhub.com/graphql" jsonBody decoder
 
 
 skipDeleted :
@@ -124,6 +223,50 @@ userDecoder =
         |> Pipeline.optional "about" (Decode.nullable Decode.string) Nothing
 
 
+field : String -> List Field -> Field
+field name fields =
+    Field name [] (Query fields)
+
+
+fieldWithArgs : String -> Args -> List Field -> Field
+fieldWithArgs name args fields =
+    Field name args (Query fields)
+
+
+queryToString : Query -> String
+queryToString (Query query) =
+    if List.isEmpty query then
+        ""
+    else
+        let
+            str =
+                List.map fieldToString query
+                    |> List.foldr (++) ""
+        in
+            "{ " ++ str ++ " }"
+
+
+fieldToString : Field -> String
+fieldToString { name, args, query } =
+    name ++ " " ++ argsToString args ++ " " ++ queryToString query
+
+
+argsToString : Args -> String
+argsToString args =
+    if List.isEmpty args then
+        ""
+    else
+        let
+            transform =
+                \( key, value ) -> key ++ ": " ++ value
+
+            str =
+                List.map transform args
+                    |> List.foldr (++) " "
+        in
+            "( " ++ str ++ " )"
+
+
 storyTypeString : StoryType -> String
 storyTypeString storyType =
     case storyType of
@@ -141,146 +284,3 @@ storyTypeString storyType =
 
         Jobs ->
             "jobStories"
-
-
-storiesQuery : StoryType -> Int -> Query
-storiesQuery storyType offset =
-    Query
-        [ field "hn"
-            [ fieldWithArgs (storyTypeString storyType)
-                [ ( "offset", toString offset ), ( "limit", "30" ) ]
-                [ field "id" []
-                , field "url" []
-                , field "title" []
-                , field "score" []
-                , field "time" []
-                , field "by"
-                    [ field "id" []
-                    ]
-                , field "descendants" []
-                , field "deleted" []
-                , field "dead" []
-                ]
-            ]
-        ]
-
-
-fetchStories : StoryType -> Int -> Http.Request (List Story)
-fetchStories storyType offset =
-    let
-        query =
-            queryToString <| storiesQuery storyType offset
-
-        jsonBody =
-            Http.jsonBody <|
-                Encode.object
-                    [ ( "query", Encode.string query ) ]
-
-        skipNull =
-            Decode.map MaybeX.values
-
-        finalDecoder =
-            Decode.list (Decode.nullable storyDecoder)
-                |> skipNull
-                |> skipDeleted
-
-        decoder =
-            Decode.at [ "data", "hn", (storyTypeString storyType) ] finalDecoder
-    in
-        Http.post "https://www.graphqlhub.com/graphql" jsonBody decoder
-
-
-commentFields : List Field
-commentFields =
-    [ field "id" []
-    , field "text" []
-    , field "score" []
-    , field "time" []
-    , field "by"
-        [ field "id" []
-        ]
-    , field "deleted" []
-    , field "dead" []
-    ]
-
-
-fetchKids : Int -> Field
-fetchKids count =
-    let
-        fields =
-            if count == 0 then
-                commentFields
-            else
-                fetchKids (count - 1) :: commentFields
-    in
-        field "kids" fields
-
-
-storyQuery : String -> Query
-storyQuery id =
-    Query
-        [ field "hn"
-            [ fieldWithArgs "item"
-                [ ( "id", id ) ]
-                [ field "id" []
-                , field "url" []
-                , field "title" []
-                , field "score" []
-                , field "time" []
-                , field "by"
-                    [ field "id" []
-                    ]
-                , fetchKids 5
-                , field "deleted" []
-                , field "dead" []
-                ]
-            ]
-        ]
-
-
-fetchStory : String -> Http.Request Story
-fetchStory id =
-    let
-        query =
-            queryToString <| storyQuery id
-
-        jsonBody =
-            Http.jsonBody <|
-                Encode.object
-                    [ ( "query", Encode.string query ) ]
-
-        decoder =
-            Decode.at [ "data", "hn", "item" ] storyDecoder
-    in
-        Http.post "https://www.graphqlhub.com/graphql" jsonBody decoder
-
-
-userQuery : String -> Query
-userQuery id =
-    Query
-        [ field "hn"
-            [ fieldWithArgs "user"
-                [ ( "id", toString id ) ]
-                [ field "id" []
-                , field "created" []
-                , field "about" []
-                ]
-            ]
-        ]
-
-
-fetchUser : String -> Http.Request User
-fetchUser id =
-    let
-        query =
-            queryToString <| userQuery id
-
-        jsonBody =
-            Http.jsonBody <|
-                Encode.object
-                    [ ( "query", Encode.string query ) ]
-
-        decoder =
-            Decode.at [ "data", "hn", "user" ] userDecoder
-    in
-        Http.post "https://www.graphqlhub.com/graphql" jsonBody decoder
