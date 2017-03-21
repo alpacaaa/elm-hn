0b9ad08b75d38cf134261cf06088c8505d83acbe
Marco Sampellegrini <babbonatale@alpacaaa.net>
2017-03-21 10:22:02 +0100

    Reorder functions so that it's easier to grasp what's going on.

--- src/App.elm
+++ src/App.elm
@@ -20,112 +20,19 @@ type alias HeaderLinkConfig =
     }
 
 
-notFoundView : Html Msg
-notFoundView =
-    genericView
-        [ div [ class "Item__title" ]
-            [ text "Not Found" ]
-        ]
-
-
-genericView : List (Html Msg) -> Html Msg
-genericView content =
-    div [ class "Items" ]
-        [ ol [ class "Items__list" ]
-            [ div [ class "Item" ] content
+view : Model -> Html Msg
+view model =
+    div [ class "App" ]
+        [ div [ class "App__wrap" ]
+            [ header model.route
+            , div [ class "App__content" ]
+                [ mainContent model ]
+            , div [ class "App__footer" ]
+                [ a [ Html.Attributes.href "https://github.com/alpacaaa/elm-hn" ]
+                    [ text "elm-hn" ]
+                ]
             ]
         ]
-
-
-loadingView : Html Msg
-loadingView =
-    genericView
-        [ div [ class "Item__title" ] [ text "Loading..." ]
-        , p [] [ text "api is a bit slow, hold on." ]
-        ]
-
-
-errorView : String -> Html Msg
-errorView err =
-    genericView
-        [ text "Good News! Something blew up ¯\\_(ツ)_/¯ Here's the error."
-        , pre []
-            [ code [] [ text err ]
-            ]
-        ]
-
-
-remoteContent : WebData a -> (a -> Html Msg) -> Html Msg
-remoteContent data createHtml =
-    case data of
-        NotAsked ->
-            text ""
-
-        Loading ->
-            loadingView
-
-        Success a ->
-            createHtml a
-
-        Failure err ->
-            errorView (toString err)
-
-
-remoteConentStories : StoryList -> Time -> Html Msg
-remoteConentStories { page, stories } now =
-    remoteContent stories (Stories.page { now = now, page = page })
-
-
-mainContent : Model -> Html Msg
-mainContent model =
-    case model.route of
-        StoriesPageRoute storyType data ->
-            remoteConentStories data model.now
-
-        StoryRoute { story, collapsedComments } ->
-            remoteContent story (SingleStory.page { now = model.now, collapsedComments = collapsedComments })
-
-        UserRoute { user } ->
-            remoteContent user (UserProfile.page model.now)
-
-        NotFoundRoute ->
-            notFoundView
-
-
-headerLink : Maybe StoryType -> HeaderLinkConfig -> Html Msg
-headerLink activeStoryType config =
-    let
-        link =
-            href config.path
-
-        isActive =
-            Maybe.unwrap False ((==) config.storyType) activeStoryType
-
-        classes =
-            config.class
-                ++ (if isActive then
-                        " active"
-                    else
-                        ""
-                   )
-    in
-        a (link ++ [ class classes ]) [ text config.text ]
-
-
-routeToStoryType : Route -> Maybe StoryType
-routeToStoryType route =
-    case route of
-        StoriesPageRoute storyType _ ->
-            Just storyType
-
-        StoryRoute _ ->
-            Nothing
-
-        UserRoute _ ->
-            Nothing
-
-        NotFoundRoute ->
-            Nothing
 
 
 header : Route -> Html Msg
@@ -181,16 +88,109 @@ header route =
                 ++ navLinks
 
 
-view : Model -> Html Msg
-view model =
-    div [ class "App" ]
-        [ div [ class "App__wrap" ]
-            [ header model.route
-            , div [ class "App__content" ]
-                [ mainContent model ]
-            , div [ class "App__footer" ]
-                [ a [ Html.Attributes.href "https://github.com/alpacaaa/elm-hn" ]
-                    [ text "elm-hn" ]
-                ]
+routeToStoryType : Route -> Maybe StoryType
+routeToStoryType route =
+    case route of
+        StoriesPageRoute storyType _ ->
+            Just storyType
+
+        StoryRoute _ ->
+            Nothing
+
+        UserRoute _ ->
+            Nothing
+
+        NotFoundRoute ->
+            Nothing
+
+
+headerLink : Maybe StoryType -> HeaderLinkConfig -> Html Msg
+headerLink activeStoryType config =
+    let
+        link =
+            href config.path
+
+        isActive =
+            Maybe.unwrap False ((==) config.storyType) activeStoryType
+
+        classes =
+            config.class
+                ++ (if isActive then
+                        " active"
+                    else
+                        ""
+                   )
+    in
+        a (link ++ [ class classes ]) [ text config.text ]
+
+
+mainContent : Model -> Html Msg
+mainContent model =
+    case model.route of
+        StoriesPageRoute storyType data ->
+            remoteConentStories data model.now
+
+        StoryRoute { story, collapsedComments } ->
+            remoteContent story (SingleStory.page { now = model.now, collapsedComments = collapsedComments })
+
+        UserRoute { user } ->
+            remoteContent user (UserProfile.page model.now)
+
+        NotFoundRoute ->
+            notFoundView
+
+
+remoteContent : WebData a -> (a -> Html Msg) -> Html Msg
+remoteContent data createHtml =
+    case data of
+        NotAsked ->
+            text ""
+
+        Loading ->
+            loadingView
+
+        Success a ->
+            createHtml a
+
+        Failure err ->
+            errorView (toString err)
+
+
+remoteConentStories : StoryList -> Time -> Html Msg
+remoteConentStories { page, stories } now =
+    remoteContent stories (Stories.page { now = now, page = page })
+
+
+genericView : List (Html Msg) -> Html Msg
+genericView content =
+    div [ class "Items" ]
+        [ ol [ class "Items__list" ]
+            [ div [ class "Item" ] content
+            ]
+        ]
+
+
+notFoundView : Html Msg
+notFoundView =
+    genericView
+        [ div [ class "Item__title" ]
+            [ text "Not Found" ]
+        ]
+
+
+loadingView : Html Msg
+loadingView =
+    genericView
+        [ div [ class "Item__title" ] [ text "Loading..." ]
+        , p [] [ text "api is a bit slow, hold on." ]
+        ]
+
+
+errorView : String -> Html Msg
+errorView err =
+    genericView
+        [ text "Good News! Something blew up ¯\\_(ツ)_/¯ Here's the error."
+        , pre []
+            [ code [] [ text err ]
             ]
         ]
